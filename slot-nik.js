395e731c21c4c892dfa6d8697d2d1c40e2516636
bahrus <anderson.bruce.b@gmail.com>
2021-05-22 07:56:03 -0400

    ...

---
+++ slot-nik.js
@@ -0,0 +1,6 @@
+import { XtF } from 'xt-f/xt-f.js';
+import { xc } from 'xtal-element/lib/XtalCore.js';
+export class SlotNik extends XtF {
+}
+SlotNik.is = 'slot-nik';
+xc.define(SlotNik);
---
+++ slot-nik.ts
@@ -0,0 +1,7 @@
+import {XtF} from 'xt-f/xt-f.js';
+import {xc} from 'xtal-element/lib/XtalCore.js';
+
+export class SlotNik extends XtF{
+    static is = 'slot-nik';
+}
+xc.define(SlotNik);
